850c74266d76b174a48fc361d300279d4d178251
Joseph cohen <jojocohen57@gmail.com>
2020-10-06 20:32:11 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 // Code your solutions in this file
 function writeCards(array, event){
   for (let i = 0; i < array.length; i++) 
-  puts 'Thank you, ${name}, for the wonderful birthday gift!'
+  console.log('Thank you, ${name}, for the wonderful birthday gift!');
 }
