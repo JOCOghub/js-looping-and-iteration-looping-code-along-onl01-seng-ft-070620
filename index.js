41b546af7f2339ec22d8e66bfa073548e1c2c520
Joseph cohen <jojocohen57@gmail.com>
2020-10-06 20:35:03 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,5 +1,15 @@
-// Code your solutions in this file
-function writeCards(array, event){
-  for (let i = 0; i < array.length; i++) 
-  console.log('Thank you, ${name}, for the wonderful birthday gift!');
+function writeCards( namesArray, event ) {
+  let thankYouCards = []
+  for ( let i = 0; i < namesArray.length; i++ ) {
+    thankYouCards.push( `Thank you, ${namesArray[i]}, for the wonderful ${event} gift!` )
+  }
+  return thankYouCards
+}
+
+function countDown( startingNumber ) {
+  while ( startingNumber > 0 ) {
+    console.log( startingNumber );
+    startingNumber -= 1;
+  }
+  console.log( startingNumber );
 }
